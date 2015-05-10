4871618ba139fb26ed5a424336b4d4fa6b1d78c0
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2015-05-10 18:29:32 +0200

    implemented header and signature parsing

--- headers.go
+++ headers.go
@@ -1,17 +1,44 @@
 package gokeepass_lib
 
+import "fmt"
+
 // Headers holds the header information of the Keepass File.
 type Headers struct {
-	Comment             uint32 // FieldID:  1
-	CipherID            uint32 // FieldID:  2
-	CompressionFlags    []byte // FieldID:  3
-	MasterSeed          uint32 // FieldID:  4
-	TransaformSeed      uint32 // FieldID:  5
-	TransformRounds     []byte // FieldID:  6
-	EncryptionIV        uint32 // FieldID:  7
-	ProtectedStreamKey  uint32 // FieldID:  8
-	StreamStartBytes    uint32 // FieldID:  9
-	InnerRandomStreamID uint32 // FieldID: 10
+	Comment             []byte // FieldID:  1
+	CipherID            []byte // FieldID:  2
+	CompressionFlags    uint32 // FieldID:  3
+	MasterSeed          []byte // FieldID:  4
+	TransformSeed       []byte // FieldID:  5
+	TransformRounds     uint32 // FieldID:  6
+	EncryptionIV        []byte // FieldID:  7
+	ProtectedStreamKey  []byte // FieldID:  8
+	StreamStartBytes    []byte // FieldID:  9
+	InnerRandomStreamID []byte // FieldID: 10
 }
 
 // 0: EndOfHeader
+
+func (h Headers) String() string {
+	return fmt.Sprintf(
+		"(1) Comment: %s\n"+
+			"(2) CipherID: %s\n"+
+			"(3) CompressionFlags: %t\n"+
+			"(4) MasterSeed: %s\n"+
+			"(5) TransformSeed: %s\n"+
+			"(6) TransformRounds: %t\n"+
+			"(7) EncryptionIV: %s\n"+
+			"(8) ProtectedStreamKey: %s\n"+
+			"(9) StreamStartBytes: %s\n"+
+			"(10) InnerRandomStreamID: %s\n",
+		h.Comment,
+		h.CipherID,
+		h.CompressionFlags,
+		h.MasterSeed,
+		h.TransformSeed,
+		h.TransformRounds,
+		h.EncryptionIV,
+		h.ProtectedStreamKey,
+		h.StreamStartBytes,
+		h.InnerRandomStreamID,
+	)
+}
--- signature.go
+++ signature.go
@@ -1,11 +1,21 @@
 package gokeepass_lib
 
+import "fmt"
+
 // Signature holds the Keepass File Signature.
 // The first 4 Bytes are the Base Signature,
 // followed by 4 Bytes for the Version of the Format
 // which is followed by 4 Bytes for the File Version
 type Signature struct {
-	BaseSignature    uint32
-	VersionSignature uint32
-	FileVersion      uint32
+	BaseSignature    [4]byte
+	VersionSignature [4]byte
+	FileVersion      [4]byte
+}
+
+func (s Signature) String() string {
+	return fmt.Sprintf("Base: %t, Version: %t, FileVersion: %t",
+		s.BaseSignature,
+		s.VersionSignature,
+		s.FileVersion,
+	)
 }
