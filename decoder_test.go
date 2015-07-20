c905e49d4085dec83f5116b7138f64742e361a3e
Kevin Michael Allen <kma1660@gmail.com>
2015-07-19 20:20:46 -0400

    added support for file attachments, and added test for this
    example.kdbx is changed to include a small binary file which says 'Hello world' for testing

--- decoder_test.go
+++ decoder_test.go
@@ -17,9 +17,22 @@ func TestDecodeFile(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to decode file: %s", err)
 	}
-
+	
+	//Tests out the binary file in example.kdbx
+	binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db.Content.Meta.Binaries)
+	if binary == nil {
+		t.Fatalf("Failed to find binary")
+	}
+	str,err := binary.GetContent()
+	if err != nil {
+		t.Fatal("Error getting content from binary: ",err,str)
+	}
+	if str != "Hello world" {
+		t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`",str)
+	}
+	
 	db.UnlockProtectedEntries()
-	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].Password
+	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
 	if string(pw) != "Password" {
 		t.Fatalf(
 			"Failed to decode password: should be 'Password' not '%s'",
@@ -31,6 +44,9 @@ func TestDecodeFile(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to open file for writing: %s", err)
 	}
+
+	//Changes the value of a entry element to see if the change stays after decoding
+	db.Content.Root.Groups[0].Groups[0].Entries[0].Get("URL").Value.Content = "http://github.com"
 
 	enc := NewEncoder(f)
 	enc.Encode(db)
@@ -48,11 +64,19 @@ func TestDecodeFile(t *testing.T) {
 	}
 
 	db.UnlockProtectedEntries()
-	pw = db.Content.Root.Groups[0].Groups[0].Entries[0].Password
-	if string(pw) != "Password" {
+	pw = db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
+	if pw != "Password" {
 		t.Fatalf(
 			"Failed to decode password: should be 'Password' not '%s'",
 			pw,
+		)
+	}
+	
+	url := db.Content.Root.Groups[0].Groups[0].Entries[0].GetContent("URL")
+	if url != "http://github.com" {
+		t.Fatalf(
+			"Failed to decode url: should be 'http://github.com' not '%s'",
+			url,
 		)
 	}
 }
