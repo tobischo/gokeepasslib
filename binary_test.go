bbea835618243a7cd1648c8914b6ecef75ba4205
Kevin Michael Allen <kma1660@gmail.com>
2015-07-26 19:51:39 -0400

    should be Fatalf, not fatal

--- binary_test.go
+++ binary_test.go
@@ -22,14 +22,14 @@ func TestBinary(t *testing.T) {
 	references := []BinaryReference{}
 	references = append(references, binary.CreateReference("example.txt"))
 	if references[0].Value.ID != 4 {
-		t.Fatal("Binary Reference ID is inncorrect. Should be 4, was %d", references[0].Value.ID)
+		t.Fatalf("Binary Reference ID is inncorrect. Should be 4, was %d", references[0].Value.ID)
 	}
 	if str, _ := references[0].Find(binaries).GetContent(); str != "test" {
-		t.Fatal("Binary Reference GetContent is inncorrect. Should be `test`, was '%s'", str)
+		t.Fatalf("Binary Reference GetContent is inncorrect. Should be `test`, was '%s'", str)
 	}
 
 	found := binaries.Find(binary2.ID)
 	if str, _ := found.GetContent(); str != "Hello world!" {
-		t.Fatal("Binary content from Find is inncorrect. Should be `Hello world!`, was '%s'", str)
+		t.Fatalf("Binary content from Find is inncorrect. Should be `Hello world!`, was '%s'", str)
 	}
 }
