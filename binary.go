9b1ec7ea1e2d6eab5fee192e2747d334c9ce0b44
Kevin Michael Allen <kma1660@gmail.com>
2015-07-26 02:17:36 -0400

    test for binary set and get content

--- binary.go
+++ binary.go
@@ -1,48 +1,20 @@
 package gokeepasslib
 
 import (
-	"fmt"
 	"bytes"
 	"compress/gzip"
 	"encoding/base64"
-	"io/ioutil"
+	"fmt"
 	"io"
+	"io/ioutil"
 )
 
 //Binaries Stores a slice of binaries in the metadata header of a database
 type Binaries []Binary
 
-//Binary stores a binary found in the metadata header of a database
-type Binary struct {
-	Content    []byte     `xml:",innerxml"`
-	ID         int         `xml:"ID,attr"`
-	Compressed boolWrapper `xml:"Compressed,attr"`
-}
-
-func (b Binary) String () (string) {
-	return fmt.Sprintf("ID: %d, Compressed:%t, Content:%x",b.ID,b.Compressed,b.Content)
-}
-
-//BinaryReference stores a reference to a binary which appears in the xml of an entry
-type BinaryReference struct {
-	Name	 string	   `xml:"Key"`
-	Value struct { 
-		ID     int	`xml:"Ref,attr"`
-	} `xml:"Value"`  
-}
-
-func (br BinaryReference) String () (string) {
-	return fmt.Sprintf("ID: %d, File Name: %s",br.Value.ID,br.Name)
-}
-
-//Find returns a reference to  a binary in the slice of binaries bs with the same id as br, or nil if none is found
-func (br *BinaryReference) Find (bs Binaries) (*Binary) {
-	return bs.Find(br.Value.ID)
-}
-
 //Find returns a reference to a binary with the same ID as id, or nil if none if found
-func (bs Binaries) Find (id int) (*Binary) {
-	for i,_ := range bs {
+func (bs Binaries) Find(id int) *Binary {
+	for i, _ := range bs {
 		if bs[i].ID == id {
 			return &bs[i]
 		}
@@ -50,24 +22,80 @@ func (bs Binaries) Find (id int) (*Binary) {
 	return nil
 }
 
+//Binary stores a binary found in the metadata header of a database
+type Binary struct {
+	Content    []byte      `xml:",innerxml"`
+	ID         int         `xml:"ID,attr"`
+	Compressed boolWrapper `xml:"Compressed,attr"`
+}
+
+func (b Binary) String() string {
+	return fmt.Sprintf("ID: %d, Compressed:%t, Content:%x", b.ID, b.Compressed, b.Content)
+}
+
 //GetContent returns a string which is the plaintext content of a binary, may return an error if something goes wrong in decoding from base64 or decompressing
-func (b Binary) GetContent () (string,error) {
-	decoded := make([]byte,base64.StdEncoding.DecodedLen(len(b.Content)))
-	_,err := base64.StdEncoding.Decode(decoded,b.Content)
+func (b Binary) GetContent() (string, error) {
+	decoded := make([]byte, base64.StdEncoding.DecodedLen(len(b.Content)))
+	_, err := base64.StdEncoding.Decode(decoded, b.Content)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 	if b.Compressed {
-		reader,err := gzip.NewReader(bytes.NewReader(decoded))
+		reader, err := gzip.NewReader(bytes.NewReader(decoded))
 		if err != nil {
-			return "",err
+			return "", err
 		}
 		defer reader.Close()
 		bts, err := ioutil.ReadAll(reader)
 		if err != nil && err != io.ErrUnexpectedEOF {
-			return "",err
+			return "", err
 		}
 		return string(bts), nil
 	}
-	return string(decoded),nil
+	return string(decoded), nil
+}
+
+//SetContent encodes and (if Compressed=true) compresses c and sets b's content
+func (b *Binary) SetContent(c []byte) error {
+	buff := &bytes.Buffer{}
+	writer := base64.NewEncoder(base64.StdEncoding, buff)
+	if b.Compressed {
+		writer = gzip.NewWriter(writer)
+	}
+	_, err := writer.Write(c)
+	if err != nil {
+		return err
+	}
+	writer.Close()
+	b.Content = buff.Bytes()
+	return nil
+}
+
+//CreateReference creates a reference with the same id as b with filename f
+func (b Binary) CreateReference(f string) BinaryReference {
+	return NewBinaryReference(f, b.ID)
+}
+
+//BinaryReference stores a reference to a binary which appears in the xml of an entry
+type BinaryReference struct {
+	Name  string `xml:"Key"`
+	Value struct {
+		ID int `xml:"Ref,attr"`
+	} `xml:"Value"`
+}
+
+func NewBinaryReference(name string, id int) BinaryReference {
+	ref := BinaryReference{}
+	ref.Name = name
+	ref.Value.ID = id
+	return ref
+}
+
+func (br BinaryReference) String() string {
+	return fmt.Sprintf("ID: %d, File Name: %s", br.Value.ID, br.Name)
+}
+
+//Find returns a reference to  a binary in the slice of binaries bs with the same id as br, or nil if none is found
+func (br *BinaryReference) Find(bs Binaries) *Binary {
+	return bs.Find(br.Value.ID)
 }
