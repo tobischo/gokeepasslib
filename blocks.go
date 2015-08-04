2c91ff3276f21b02eac96715e7bc39781b126789
Kevin Michael Allen <kma1660@gmail.com>
2015-08-03 21:00:37 -0400

    comments for blocks.go

--- blocks.go
+++ blocks.go
@@ -8,9 +8,10 @@ import (
 	"bytes"
 )
 
-//Size in bytes of the data in each block
+// Size in bytes of the data in each block
 const blockSplitRate = 16384
 
+// ErrCorruptBlock is returned by Decoder when a block in the kdbx file has a different hash from the correct hash
 type ErrCorruptBlock struct {
 	block *Block
 	Hash [32]byte
@@ -19,25 +20,33 @@ func (e ErrCorruptBlock) Error () string {
 	return fmt.Sprintf("Hash mismatch at block %d. Should be %x, is %x",e.block.Index,e.Hash,e.block.Hash)
 }
 
+// ErrBlockTooSmall is returned by decoder if the block length does not match the body size
 var ErrBlockTooSmall = errors.New("gokeepasslib: block is too small")
+
+// ErrEmptyBlock is returned by decoder when a block is empty,ignored if is the last block in the file
 var ErrEmptyBlock = errors.New("gokeepasslib: block appears to be empty/clossing block")
 
-
+//minsize is the smallest size a block can be
 const minsize int = 40
 
+// Block stores an individual block following the kdbx block format
 type Block struct {
 	Index  uint32
 	Hash   [32]byte
 	Length uint32
 	Body   []byte
 }
-func NewBlock (i uint32, body []byte) (b Block) {
+
+// NewBlock creates a block with index i and body b, adding length and hash automaticaly
+func NewBlock (i uint32, b []byte) (b Block) {
 	b.Index = i
-	b.Body = body
+	b.Body = b
 	b.Hash = sha256.Sum256(b.Body)
 	b.Length = uint32(len(b.Body))
 	return
 }
+
+// MarshalText converts a block into kdbx binary block format
 func (u Block) MarshalText () (text []byte,err error) {
 	text = make([]byte,u.Size())
 	binary.LittleEndian.PutUint32(text[:4],u.Index)
@@ -46,6 +55,8 @@ func (u Block) MarshalText () (text []byte,err error) {
 	copy(text[40:],u.Body)
 	return
 }
+
+// UnmarshalText turns text in binary kdbx format into a block
 func (b *Block) UnmarshalText(text []byte) error {
 	if len (text) < minsize {
 		return ErrBlockTooSmall
@@ -66,6 +77,8 @@ func (b *Block) UnmarshalText(text []byte) error {
 	}
 	return nil
 }
+
+// Size returns the number of bytes b will occupy when Marshaled to binary
 func (b Block) Size () int {
 	return 40+len(b.Body)
 }
@@ -73,6 +86,7 @@ func (b Block) String () string {
 	return fmt.Sprintf("Index: %d\nHash: %x\n,Length:%d\n\n",b.Index,b.Hash,b.Length)
 }
 
+// DecodeBlocks converts a []byte in kdbx block format into the xml content, checking for corruption
 func DecodeBlocks (body []byte) (result []byte,err error) {
 	var block Block
 	for len(body) > 0 {
@@ -89,7 +103,7 @@ func DecodeBlocks (body []byte) (result []byte,err error) {
 	return
 }
 
-// Converts raw xml data to keepass's block format, which includes a hash of each block to check for data corruption,
+// EncodeBlocks Converts raw xml data to keepass's block format, which includes a hash of each block to check for data corruption,
 // Every block contains the following elements:
 // (4 bytes) ID : an unique interger id for this block
 // (32 bytes) sha-256 hash of block data
