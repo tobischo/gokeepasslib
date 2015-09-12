6d1a2ff924a749afaf9373ce66bbc37516dab2bf
Takuya Arita <takuya.arita@gmail.com>
2015-09-12 23:42:20 +0900

    Fix README.md and add example code to example directory

---
+++ examples/example.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tobischo/gokeepasslib"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("./example.kdbx")
+
+	db := gokeepasslib.NewDatabase()
+	db.Credentials = gokeepasslib.NewPasswordCredentials("abcdefg12345678")
+	_ = gokeepasslib.NewDecoder(file).Decode(db)
+
+	db.UnlockProtectedEntries()
+
+	entry := db.Content.Root.Groups[0].Groups[0].Entries[0]
+	fmt.Println(entry.GetTitle())
+	fmt.Println(string(entry.GetPassword()))
+}
