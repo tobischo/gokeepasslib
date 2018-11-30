2525b57dd6ecad25884f006ad232a041d3a3e510
AlessioDP <alessiodptv@gmail.com>
2018-11-30 22:06:21 +0100

    Implemented test unit for ChaCha20 database

--- decoder_test.go
+++ decoder_test.go
@@ -291,3 +291,58 @@ func TestDecodeFile4_NoCompression(t *testing.T) {
 		)
 	}
 }
+
+// Decode database v4.0 with ChaCha encryption
+func TestDecodeFile4_ChaCha(t *testing.T) {
+	// Open file
+	file, err := os.Open("tests/kdbx4/example-chacha.kdbx")
+	if err != nil {
+		t.Fatalf("Failed to open keepass file: %s", err)
+	}
+	defer file.Close()
+
+	// Decode database
+	db := NewDatabase()
+	db.Credentials = NewPasswordCredentials("abcdefg12345678")
+	err = NewDecoder(file).Decode(db)
+	if err != nil {
+		t.Fatalf("Failed to decode file: %s", err)
+	}
+
+	// Test binary file matching
+	binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db)
+	if binary == nil {
+		t.Fatalf("Failed to find binary")
+	}
+	str, err := binary.GetContent()
+	if err != nil {
+		t.Fatal("Error getting content from binary: ", err, str)
+	}
+	if str != "Hello world" {
+		t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`", str)
+	}
+
+	// Unlock entries
+	err = db.UnlockProtectedEntries()
+	if err != nil {
+		t.Fatalf("Problem unlocking entries. %s", err)
+	}
+
+	// Test password matching
+	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
+	if pw != "Password" {
+		t.Fatalf(
+			"Failed to decode password: should be 'Password' not '%s'",
+			pw,
+		)
+	}
+
+	// Test secondary password matching
+	pw = db.Content.Root.Groups[0].Groups[0].Entries[1].GetPassword()
+	if pw != "AnotherPassword" {
+		t.Fatalf(
+			"Failed to decode password: should be 'AnotherPassword' not '%s'",
+			pw,
+		)
+	}
+}
