1f9275c72b85b51ae7fc3dcd5b14760c9be8a0c3
Tobias Schoknecht <tobias.schoknecht@barzahlen.de>
2018-12-03 20:10:18 +0100

    Change decoder tests to table based tests
    They are almost identical anyway
    
    Also add argon2 example

--- decoder_test.go
+++ decoder_test.go
@@ -5,344 +5,126 @@ import (
 	"testing"
 )
 
-// Decode database v3.1
-func TestDecodeFile31(t *testing.T) {
-	// Open file
-	file, err := os.Open("tests/kdbx3/example.kdbx")
-	if err != nil {
-		t.Fatalf("Failed to open keepass file: %s", err)
-	}
-	defer file.Close()
-
-	// Decode database
-	db := NewDatabase()
-	db.Credentials = NewPasswordCredentials("abcdefg12345678")
-	err = NewDecoder(file).Decode(db)
-	if err != nil {
-		t.Fatalf("Failed to decode file: %s", err)
-	}
-
-	// Test binary file matching
-	binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db)
-	if binary == nil {
-		t.Fatalf("Failed to find binary")
-	}
-	str, err := binary.GetContent()
-	if err != nil {
-		t.Fatal("Error getting content from binary: ", err, str)
-	}
-	if str != "Hello world" {
-		t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`", str)
-	}
-
-	// Unlock entries
-	err = db.UnlockProtectedEntries()
-	if err != nil {
-		t.Fatalf("Problem unlocking entries. %s", err)
-	}
-
-	// Test password matching
-	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
-	if pw != "Password" {
-		t.Fatalf(
-			"Failed to decode password: should be 'Password' not '%s'",
-			pw,
-		)
-	}
-
-	// Test secondary password matching
-	pw = db.Content.Root.Groups[0].Groups[0].Entries[1].GetPassword()
-	if pw != "AnotherPassword" {
-		t.Fatalf(
-			"Failed to decode password: should be 'AnotherPassword' not '%s'",
-			pw,
-		)
-	}
-}
-
-// Decode database v4.0
-func TestDecodeFile4(t *testing.T) {
-	// Open file
-	file, err := os.Open("tests/kdbx4/example.kdbx")
-	if err != nil {
-		t.Fatalf("Failed to open keepass file: %s", err)
-	}
-	defer file.Close()
-
-	// Decode database
-	db := NewDatabase()
-	db.Credentials = NewPasswordCredentials("abcdefg12345678")
-	err = NewDecoder(file).Decode(db)
-	if err != nil {
-		t.Fatalf("Failed to decode file: %s", err)
+func TestDecodeFile(t *testing.T) {
+	cases := []struct {
+		title          string
+		dbFilePath     string
+		newCredentials func() (*DBCredentials, error)
+	}{
+		{
+			title:      "Database Format v3.1, password credentials",
+			dbFilePath: "tests/kdbx3/example.kdbx",
+			newCredentials: func() (*DBCredentials, error) {
+				return NewPasswordCredentials("abcdefg12345678"), nil
+			},
+		},
+		{
+			title:      "Database Format v3.1, password+key credentials",
+			dbFilePath: "tests/kdbx3/example-key.kdbx",
+			newCredentials: func() (*DBCredentials, error) {
+				return NewPasswordAndKeyCredentials(
+					"abcdefg12345678",
+					"tests/kdbx3/example-key.key",
+				)
+			},
+		},
+		{
+			title:      "Database Format v4, password credentials",
+			dbFilePath: "tests/kdbx4/example.kdbx",
+			newCredentials: func() (*DBCredentials, error) {
+				return NewPasswordCredentials("abcdefg12345678"), nil
+			},
+		},
+		{
+			title:      "Database Format v4, password+key credentials",
+			dbFilePath: "tests/kdbx4/example-key.kdbx",
+			newCredentials: func() (*DBCredentials, error) {
+				return NewPasswordAndKeyCredentials(
+					"abcdefg12345678",
+					"tests/kdbx4/example-key.key",
+				)
+			},
+		},
+		{
+			title:      "Database Format v4, without compression, password credentials",
+			dbFilePath: "tests/kdbx4/example-nocompression.kdbx",
+			newCredentials: func() (*DBCredentials, error) {
+				return NewPasswordCredentials("abcdefg12345678"), nil
+			},
+		},
+		{
+			title:      "Database Format v4, chacha encryption, password credentials",
+			dbFilePath: "tests/kdbx4/example-chacha.kdbx",
+			newCredentials: func() (*DBCredentials, error) {
+				return NewPasswordCredentials("abcdefg12345678"), nil
+			},
+		},
+		{
+			title:      "Database Format v4, chacha encryption, argon2 key transformation, password credentials",
+			dbFilePath: "tests/kdbx4/example-chacha-argon2.kdbx",
+			newCredentials: func() (*DBCredentials, error) {
+				return NewPasswordCredentials("abcdefg12345678"), nil
+			},
+		},
 	}
 
-	// Test binary file matching
-	binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db)
-	if binary == nil {
-		t.Fatalf("Failed to find binary")
-	}
-	str, err := binary.GetContent()
-	if err != nil {
-		t.Fatal("Error getting content from binary: ", err, str)
-	}
-	if str != "Hello world" {
-		t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`", str)
-	}
+	for _, c := range cases {
+		t.Run(c.title, func(t *testing.T) {
+			// Open file
+			file, err := os.Open(c.dbFilePath)
+			if err != nil {
+				t.Fatalf("Failed to open keepass file: %s", err)
+			}
+			defer file.Close()
 
-	// Unlock entries
-	err = db.UnlockProtectedEntries()
-	if err != nil {
-		t.Fatalf("Problem unlocking entries. %s", err)
-	}
+			// Decode database
+			db := NewDatabase()
+			credentials, err := c.newCredentials()
+			if err != nil {
+				t.Fatalf("Failed to build new credentials: %+v", err)
+			}
+			db.Credentials = credentials
+			err = NewDecoder(file).Decode(db)
+			if err != nil {
+				t.Fatalf("Failed to decode file: %s", err)
+			}
 
-	// Test password matching
-	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
-	if pw != "Password" {
-		t.Fatalf(
-			"Failed to decode password: should be 'Password' not '%s'",
-			pw,
-		)
-	}
+			// Test binary file matching
+			binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db)
+			if binary == nil {
+				t.Fatalf("Failed to find binary")
+			}
+			str, err := binary.GetContent()
+			if err != nil {
+				t.Fatal("Error getting content from binary: ", err, str)
+			}
+			if str != "Hello world" {
+				t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`", str)
+			}
 
-	// Test secondary password matching
-	pw = db.Content.Root.Groups[0].Groups[0].Entries[1].GetPassword()
-	if pw != "AnotherPassword" {
-		t.Fatalf(
-			"Failed to decode password: should be 'AnotherPassword' not '%s'",
-			pw,
-		)
-	}
-}
+			// Unlock entries
+			err = db.UnlockProtectedEntries()
+			if err != nil {
+				t.Fatalf("Problem unlocking entries. %s", err)
+			}
 
-// Decode database v3.1 with password and key
-func TestDecodeFile31_Key(t *testing.T) {
-	// Open file
-	file, err := os.Open("tests/kdbx3/example-key.kdbx")
-	if err != nil {
-		t.Fatalf("Failed to open keepass file: %s", err)
-	}
-	defer file.Close()
+			// Test password matching
+			pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
+			if pw != "Password" {
+				t.Fatalf(
+					"Failed to decode password: should be 'Password' not '%s'",
+					pw,
+				)
+			}
 
-	// Decode database
-	db := NewDatabase()
-	db.Credentials, err = NewPasswordAndKeyCredentials(
-		"abcdefg12345678",
-		"tests/kdbx3/example-key.key",
-	)
-	if err != nil {
-		t.Fatalf("Failed to make credentials: %s", err)
-	}
-	err = NewDecoder(file).Decode(db)
-	if err != nil {
-		t.Fatalf("Failed to decode file: %s", err)
-	}
-
-	// Test binary file matching
-	binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db)
-	if binary == nil {
-		t.Fatalf("Failed to find binary")
-	}
-	str, err := binary.GetContent()
-	if err != nil {
-		t.Fatal("Error getting content from binary: ", err, str)
-	}
-	if str != "Hello world" {
-		t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`", str)
-	}
-
-	// Unlock entries
-	err = db.UnlockProtectedEntries()
-	if err != nil {
-		t.Fatalf("Problem unlocking entries. %s", err)
-	}
-
-	// Test password matching
-	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
-	if pw != "Password" {
-		t.Fatalf(
-			"Failed to decode password: should be 'Password' not '%s'",
-			pw,
-		)
-	}
-
-	// Test secondary password matching
-	pw = db.Content.Root.Groups[0].Groups[0].Entries[1].GetPassword()
-	if pw != "AnotherPassword" {
-		t.Fatalf(
-			"Failed to decode password: should be 'AnotherPassword' not '%s'",
-			pw,
-		)
-	}
-}
-
-// Decode database v4.0 with password and key
-func TestDecodeFile4_Key(t *testing.T) {
-	// Open file
-	file, err := os.Open("tests/kdbx4/example-key.kdbx")
-	if err != nil {
-		t.Fatalf("Failed to open keepass file: %s", err)
-	}
-	defer file.Close()
-
-	// Decode database
-	db := NewDatabase()
-	db.Credentials, err = NewPasswordAndKeyCredentials(
-		"abcdefg12345678",
-		"tests/kdbx4/example-key.key",
-	)
-	if err != nil {
-		t.Fatalf("Failed to make credentials: %s", err)
-	}
-	err = NewDecoder(file).Decode(db)
-	if err != nil {
-		t.Fatalf("Failed to decode file: %s", err)
-	}
-
-	// Test binary file matching
-	binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db)
-	if binary == nil {
-		t.Fatalf("Failed to find binary")
-	}
-	str, err := binary.GetContent()
-	if err != nil {
-		t.Fatal("Error getting content from binary: ", err, str)
-	}
-	if str != "Hello world" {
-		t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`", str)
-	}
-
-	// Unlock entries
-	err = db.UnlockProtectedEntries()
-	if err != nil {
-		t.Fatalf("Problem unlocking entries. %s", err)
-	}
-
-	// Test password matching
-	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
-	if pw != "Password" {
-		t.Fatalf(
-			"Failed to decode password: should be 'Password' not '%s'",
-			pw,
-		)
-	}
-
-	// Test secondary password matching
-	pw = db.Content.Root.Groups[0].Groups[0].Entries[1].GetPassword()
-	if pw != "AnotherPassword" {
-		t.Fatalf(
-			"Failed to decode password: should be 'AnotherPassword' not '%s'",
-			pw,
-		)
-	}
-}
-
-// Decode database v4.0 without compression
-func TestDecodeFile4_NoCompression(t *testing.T) {
-	// Open file
-	file, err := os.Open("tests/kdbx4/example-nocompression.kdbx")
-	if err != nil {
-		t.Fatalf("Failed to open keepass file: %s", err)
-	}
-	defer file.Close()
-
-	// Decode database
-	db := NewDatabase()
-	db.Credentials = NewPasswordCredentials("abcdefg12345678")
-	err = NewDecoder(file).Decode(db)
-	if err != nil {
-		t.Fatalf("Failed to decode file: %s", err)
-	}
-
-	// Test binary file matching
-	binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db)
-	if binary == nil {
-		t.Fatalf("Failed to find binary")
-	}
-	str, err := binary.GetContent()
-	if err != nil {
-		t.Fatal("Error getting content from binary: ", err, str)
-	}
-	if str != "Hello world" {
-		t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`", str)
-	}
-
-	// Unlock entries
-	err = db.UnlockProtectedEntries()
-	if err != nil {
-		t.Fatalf("Problem unlocking entries. %s", err)
-	}
-
-	// Test password matching
-	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
-	if pw != "Password" {
-		t.Fatalf(
-			"Failed to decode password: should be 'Password' not '%s'",
-			pw,
-		)
-	}
-
-	// Test secondary password matching
-	pw = db.Content.Root.Groups[0].Groups[0].Entries[1].GetPassword()
-	if pw != "AnotherPassword" {
-		t.Fatalf(
-			"Failed to decode password: should be 'AnotherPassword' not '%s'",
-			pw,
-		)
-	}
-}
-
-// Decode database v4.0 with ChaCha encryption
-func TestDecodeFile4_ChaCha(t *testing.T) {
-	// Open file
-	file, err := os.Open("tests/kdbx4/example-chacha.kdbx")
-	if err != nil {
-		t.Fatalf("Failed to open keepass file: %s", err)
-	}
-	defer file.Close()
-
-	// Decode database
-	db := NewDatabase()
-	db.Credentials = NewPasswordCredentials("abcdefg12345678")
-	err = NewDecoder(file).Decode(db)
-	if err != nil {
-		t.Fatalf("Failed to decode file: %s", err)
-	}
-
-	// Test binary file matching
-	binary := db.Content.Root.Groups[0].Groups[1].Entries[0].Binaries[0].Find(db)
-	if binary == nil {
-		t.Fatalf("Failed to find binary")
-	}
-	str, err := binary.GetContent()
-	if err != nil {
-		t.Fatal("Error getting content from binary: ", err, str)
-	}
-	if str != "Hello world" {
-		t.Fatalf("Binary content was not as expected, expected: `Hello world`, received `%s`", str)
-	}
-
-	// Unlock entries
-	err = db.UnlockProtectedEntries()
-	if err != nil {
-		t.Fatalf("Problem unlocking entries. %s", err)
-	}
-
-	// Test password matching
-	pw := db.Content.Root.Groups[0].Groups[0].Entries[0].GetPassword()
-	if pw != "Password" {
-		t.Fatalf(
-			"Failed to decode password: should be 'Password' not '%s'",
-			pw,
-		)
-	}
-
-	// Test secondary password matching
-	pw = db.Content.Root.Groups[0].Groups[0].Entries[1].GetPassword()
-	if pw != "AnotherPassword" {
-		t.Fatalf(
-			"Failed to decode password: should be 'AnotherPassword' not '%s'",
-			pw,
-		)
+			// Test secondary password matching
+			pw = db.Content.Root.Groups[0].Groups[0].Entries[1].GetPassword()
+			if pw != "AnotherPassword" {
+				t.Fatalf(
+					"Failed to decode password: should be 'AnotherPassword' not '%s'",
+					pw,
+				)
+			}
+		})
 	}
 }
