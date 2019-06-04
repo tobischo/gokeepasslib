66a994841e571a51bfec94fe565cc8e39afcc3f0
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2019-06-04 20:00:05 +0200

    Add example for reading and writing kdbx4 file

---
+++ examples/check_kdbx4_read_and_write.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/tobischo/gokeepasslib/v2"
+)
+
+func main() {
+	// Read
+	file, err := os.Open("./kdbx4.kdbx")
+	if err != nil {
+		log.Printf("Failed opening file: %+v", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	db := gokeepasslib.NewDatabase()
+	db.Credentials = gokeepasslib.NewPasswordCredentials("123")
+	err = gokeepasslib.NewDecoder(file).Decode(db)
+	if err != nil {
+		log.Printf("Decoding file: %+v", err)
+		os.Exit(1)
+	}
+
+	// Write
+	resultFile, err := os.Create("./kdbx4_result.kdbx")
+	if err != nil {
+		log.Printf("Failed opening file: %+v", err)
+		os.Exit(1)
+	}
+
+	err = gokeepasslib.NewEncoder(resultFile).Encode(db)
+	if err != nil {
+		log.Printf("Encoding file: %+v", err)
+		os.Exit(1)
+	}
+	resultFile.Close()
+
+	// Read again
+	file2, err := os.Open("./kdbx4_result.kdbx")
+	if err != nil {
+		log.Printf("Failed opening file: %+v", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	db2 := gokeepasslib.NewDatabase()
+	db2.Credentials = gokeepasslib.NewPasswordCredentials("123")
+	err = gokeepasslib.NewDecoder(file2).Decode(db2)
+	if err != nil {
+		log.Printf("Decoding file: %+v", err)
+		os.Exit(1)
+	}
+}
