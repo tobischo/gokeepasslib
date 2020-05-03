3ff5239f82b04803ae556a4618fce875dfdf88c4
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2020-05-03 18:54:22 +0200

    Add MetaData SetKdbxFormatVersion tests

--- meta_data_test.go
+++ meta_data_test.go
@@ -3,6 +3,9 @@ package gokeepasslib
 import (
 	"reflect"
 	"testing"
+	"time"
+
+	"github.com/tobischo/gokeepasslib/v3/wrappers"
 )
 
 func TestNewMetaData(t *testing.T) {
@@ -51,6 +54,98 @@ func TestNewMetaData(t *testing.T) {
 					c.expectedMetaData,
 					metaData,
 				)
+			}
+		})
+	}
+}
+
+func TestMetaDataSetKdbxFormatVersion(t *testing.T) {
+	cases := []struct {
+		title                  string
+		formattedInitValue     bool
+		version                formatVersion
+		expectedFormattedValue bool
+	}{
+		{
+			title:                  "initialized as v3, changed to v4",
+			formattedInitValue:     true,
+			version:                4,
+			expectedFormattedValue: false,
+		},
+		{
+			title:                  "initialized as v4, changed to v3",
+			formattedInitValue:     false,
+			version:                3,
+			expectedFormattedValue: true,
+		},
+		{
+			title:                  "initialized as v3, not changed",
+			formattedInitValue:     true,
+			version:                3,
+			expectedFormattedValue: true,
+		},
+		{
+			title:                  "initialized as v4, not changed",
+			formattedInitValue:     false,
+			version:                4,
+			expectedFormattedValue: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.title, func(t *testing.T) {
+			md := &MetaData{
+				DatabaseNameChanged: &wrappers.TimeWrapper{
+					Time:      time.Now(),
+					Formatted: c.formattedInitValue,
+				},
+				DatabaseDescriptionChanged: &wrappers.TimeWrapper{
+					Time:      time.Now(),
+					Formatted: c.formattedInitValue,
+				},
+				DefaultUserNameChanged: &wrappers.TimeWrapper{
+					Time:      time.Now(),
+					Formatted: c.formattedInitValue,
+				},
+				MasterKeyChanged: &wrappers.TimeWrapper{
+					Time:      time.Now(),
+					Formatted: c.formattedInitValue,
+				},
+				RecycleBinChanged: &wrappers.TimeWrapper{
+					Time:      time.Now(),
+					Formatted: c.formattedInitValue,
+				},
+				EntryTemplatesGroupChanged: &wrappers.TimeWrapper{
+					Time:      time.Now(),
+					Formatted: c.formattedInitValue,
+				},
+			}
+
+			md.setKdbxFormatVersion(c.version)
+
+			if md.DatabaseNameChanged.Formatted != c.expectedFormattedValue {
+
+				t.Errorf("Failed to set DatabaseNameChanged formatted value accordingly")
+			}
+			if md.DatabaseDescriptionChanged.Formatted != c.expectedFormattedValue {
+
+				t.Errorf("Failed to set DatabaseDescriptionChanged formatted value accordingly")
+			}
+			if md.DefaultUserNameChanged.Formatted != c.expectedFormattedValue {
+
+				t.Errorf("Failed to set DefaultUserNameChanged formatted value accordingly")
+			}
+			if md.MasterKeyChanged.Formatted != c.expectedFormattedValue {
+
+				t.Errorf("Failed to set MasterKeyChanged formatted value accordingly")
+			}
+			if md.RecycleBinChanged.Formatted != c.expectedFormattedValue {
+
+				t.Errorf("Failed to set RecycleBinChanged formatted value accordingly")
+			}
+			if md.EntryTemplatesGroupChanged.Formatted != c.expectedFormattedValue {
+
+				t.Errorf("Failed to set EntryTemplatesGroupChanged formatted value accordingly")
 			}
 		})
 	}
