53886c747fdb6152336f6d3459a2198eb77f0763
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2020-05-03 19:01:03 +0200

    Add entry setKdbxFormatVersion test

--- entry_test.go
+++ entry_test.go
@@ -49,3 +49,69 @@ func TestNewEntry(t *testing.T) {
 		})
 	}
 }
+
+func TestEntrySetKdbxFormatVersion(t *testing.T) {
+	cases := []struct {
+		title                  string
+		formattedInitValue     bool
+		version                formatVersion
+		expectedFormattedValue bool
+	}{
+		{
+			title:                  "initialized as v3, changed to v4",
+			formattedInitValue:     true,
+			version:                4,
+			expectedFormattedValue: false,
+		},
+		{
+			title:                  "initialized as v4, changed to v3",
+			formattedInitValue:     false,
+			version:                3,
+			expectedFormattedValue: true,
+		},
+		{
+			title:                  "initialized as v3, not changed",
+			formattedInitValue:     true,
+			version:                3,
+			expectedFormattedValue: true,
+		},
+		{
+			title:                  "initialized as v4, not changed",
+			formattedInitValue:     false,
+			version:                4,
+			expectedFormattedValue: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.title, func(t *testing.T) {
+			entry := NewEntry(
+				WithEntryFormattedTime(c.formattedInitValue),
+			)
+
+			history := History{
+				Entries: []Entry{
+					NewEntry(
+						WithEntryFormattedTime(c.formattedInitValue),
+					),
+				},
+			}
+
+			entry.Histories = append(entry.Histories, history)
+
+			(&entry).setKdbxFormatVersion(c.version)
+
+			// Takes a single time value as an example, as TimeData is independently tested.
+			if entry.Times.CreationTime.Formatted != c.expectedFormattedValue {
+
+				t.Errorf("Failed to set root CreationTime formatted value accordingly")
+			}
+
+			if entry.Histories[0].Entries[0].Times.CreationTime.Formatted != c.expectedFormattedValue {
+
+				t.Errorf("Failed to set history CreationTime formatted value accordingly")
+			}
+		})
+	}
+
+}
