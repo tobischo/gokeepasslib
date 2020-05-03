0975272eb6ee6899e5a7b3ddddeaa02a6b7d628d
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2020-05-03 18:27:12 +0200

    Move deleted object data into separate file
    xml.go is quite large and not as easy to handle.
    Splitting it up into multiple files should make that easier

--- xml.go
+++ xml.go
@@ -282,17 +282,6 @@ type AutoTypeAssociation struct {
 	KeystrokeSequence string `xml:"KeystrokeSequence"`
 }
 
-// DeletedObjectData is the structure for a deleted object
-type DeletedObjectData struct {
-	XMLName      xml.Name       `xml:"DeletedObject"`
-	UUID         UUID           `xml:"UUID"`
-	DeletionTime *w.TimeWrapper `xml:"DeletionTime"`
-}
-
-func (d *DeletedObjectData) setKdbxFormatVersion(version formatVersion) {
-	d.DeletionTime.Formatted = !isKdbx4(version)
-}
-
 // CustomData is the structure for plugins custom data
 type CustomData struct {
 	XMLName xml.Name `xml:"Item"`
