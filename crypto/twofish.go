5c21b66c56d63ef7a817529f0a9cab22168cb3f5
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2024-04-18 21:27:29 +0200

    Add twofish encryption manager

---
+++ crypto/twofish.go
@@ -0,0 +1,43 @@
+package crypto
+
+import (
+	"crypto/cipher"
+
+	"golang.org/x/crypto/twofish"
+)
+
+// TwoFishEncrypter is a TwoFish cipher that implements Encrypter interface
+type TwoFishEncrypter struct {
+	block        cipher.Block
+	encryptionIV []byte
+}
+
+// NewTwoFishEncrypter initialize a new TwoFishEncrypter interfaced with Encrypter
+func NewTwoFishEncrypter(key []byte, iv []byte) (*TwoFishEncrypter, error) {
+	block, err := twofish.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	e := TwoFishEncrypter{
+		block:        block,
+		encryptionIV: iv,
+	}
+	return &e, nil
+}
+
+// Decrypt returns the decrypted data
+func (tfe *TwoFishEncrypter) Decrypt(data []byte) []byte {
+	ret := make([]byte, len(data))
+	mode := cipher.NewCBCDecrypter(tfe.block, tfe.encryptionIV)
+	mode.CryptBlocks(ret, data)
+	return ret
+}
+
+// Encrypt returns the encrypted data
+func (tfe *TwoFishEncrypter) Encrypt(data []byte) []byte {
+	ret := make([]byte, len(data))
+	mode := cipher.NewCBCEncrypter(tfe.block, tfe.encryptionIV)
+	mode.CryptBlocks(ret, data)
+	return ret
+}
